a9651f11541f81bcecc135dbd9a25957dcfec627
SIJULY <104404467+SIJULY@users.noreply.github.com>
2025-03-30 12:03:10 +0800

    Update qubic.sh

--- qubic.sh
+++ qubic.sh
@@ -6,9 +6,13 @@ if [ -n "$CUSTOM_WORKER" ]; then
     WORKER=$CUSTOM_WORKER
 fi
 
+cd /root || exit 1  # 确保切换到正确的目录
+
 if pgrep -f 'apoolminer.*apool.io' > /dev/null; then
     echo "ApoolMiner already running."
     exit 1
 else
-    nohup ./apoolminer --account CP_0codi1cvup --worker "$WORKER" --gpu-off --pool qubic1.hk.apool.io:3334 >> qubic.log 2>&1 &
+    chmod +x /root/apoolminer  # 确保矿机可执行
+    nohup /root/apoolminer --account CP_0codi1cvup --worker "$WORKER" --gpu-off --pool qubic1.hk.apool.io:3334 >> /root/qubic.log 2>&1 &
+    echo "ApoolMiner started."
 fi
