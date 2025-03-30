745347608ee1e9cb85ba21af73ff4aa8b3cce884
SIJULY <104404467+SIJULY@users.noreply.github.com>
2025-03-30 11:58:14 +0800

    Update qubic.sh

--- qubic.sh
+++ qubic.sh
@@ -6,9 +6,9 @@ if [ -n "$CUSTOM_WORKER" ]; then
     WORKER=$CUSTOM_WORKER
 fi
 
-if ps aux | grep 'apoolminer' | grep -q 'apool.io'; then
+if pgrep -f 'apoolminer.*apool.io' > /dev/null; then
     echo "ApoolMiner already running."
     exit 1
 else
-    nohup ./apoolminer --account CP_0codi1cvup --worker $WORKER --gpu-off --pool qubic1.hk.apool.io:3334 >> qubic.log 2>&1 &
+    nohup ./apoolminer --account CP_0codi1cvup --worker "$WORKER" --gpu-off --pool qubic1.hk.apool.io:3334 >> qubic.log 2>&1 &
 fi
